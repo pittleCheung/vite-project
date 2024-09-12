ea3d01b06726b7622dd73371d447f9d75637123f
pittle <pittle.Cheung@topkee.com.hk>
2024-09-13 00:18:02 +0800

    fix: pittle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './Header';
 import './App.css';
 // import { WindowScroller } from 'react-virtualized'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <>
@@ -10,10 +11,18 @@ function App() {
         <header className="App-header">
           <img className="w-20" alt="logo" />
           <p className="bg-red-400">Hello Vite + React123!</p>
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
         </header>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" element={<div>home</div>}></Route>
+            <Route path="/about" element={<div>about</div>}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
-  );
+  )
 }
 
 export default App;
