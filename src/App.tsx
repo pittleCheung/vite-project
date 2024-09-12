bc3e276d523bbf71e2ad9a8dd509c4aa210e1445
pittle <pittle.Cheung@topkee.com.hk>
2024-09-13 00:24:21 +0800

    fix: brower router

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,23 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header></Header>
-      {/* <WindowScroller /> */}
-      <div>
-        <header className="App-header">
-          <img className="w-20" alt="logo" />
-          <p className="bg-red-400">Hello Vite + React123!</p>
-          <Link to="/home">Home</Link>
-          <Link to="/about">About</Link>
-        </header>
-        <BrowserRouter basename='/vite-project/'>
+      <BrowserRouter basename="/vite-project/">
+        <Header></Header>
+        {/* <WindowScroller /> */}
+        <div>
+          <header className="App-header">
+            <img className="w-20" alt="logo" />
+            <p className="bg-red-400">Hello Vite + React123!</p>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+          </header>
+
           <Routes>
             <Route path="/home" element={<div>home</div>}></Route>
             <Route path="/about" element={<div>about</div>}></Route>
           </Routes>
-        </BrowserRouter>
-      </div>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
